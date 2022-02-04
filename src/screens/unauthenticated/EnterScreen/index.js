8ce2a74bae0dabd03f8e213e9a94adb2c70ec117
Enrique Casanova <enrique.casanova@projekt202.com>
2022-02-04 10:27:34 -0500

    Fix iota grid

--- src/screens/unauthenticated/EnterScreen/index.js
+++ src/screens/unauthenticated/EnterScreen/index.js
@@ -8,12 +8,12 @@ function EnterScreen() {
     <div className="c-login is-enter">
       <div className="o-container u-12/12">
         <div className="o-grid o-grid--middle">
-          <div className="o-grid__col u-6/12@md">
+          <div className="o-grid__col u-6/12@sm">
             <div className="c-login__superhero-group">
               <img src={superHeroBackgroundImage} alt="" />
             </div>
           </div>
-          <div className="o-grid__col u-6/12@md">
+          <div className="o-grid__col u-6/12@sm">
             <h1 className="c-login__title">
               Create your own team of superheroes
             </h1>
