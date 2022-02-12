313e5f1a07a9a99edfd02ac47470e779c1fb0b27
Enrique Casanova <enrique.casanova@projekt202.com>
2022-02-12 17:21:07 -0500

    fix: responsive detail view

--- src/features/SuperheroCard/cardsListComponent/index.js
+++ src/features/SuperheroCard/cardsListComponent/index.js
@@ -23,32 +23,36 @@ export function Cards() {
   };
 
   return (
-    <div className="c-cards o-container o-container__texture">
-      <InfiniteScroll
-        dataLength={data.length}
-        next={() => {
-          loadMoreData();
-        }}
-        hasMore={true}>
-        <Row gutter={[25, 25]}>
-          {data?.map((superhero, index) => {
-            return (
-              <Col span={6} xs={24} md={12} l={6} xl={6} key={index}>
-                <Card superhero={superhero} key={index} />
-              </Col>
-            );
-          })}
-        </Row>
-      </InfiniteScroll>
+    <>
       {data.length === 0 && (
-        <div className="c-cards__empty">
+        <div className="c-myteam o-container o-container__texture">
           <p>
             There are no Superheros based on the filters you have selected.
             Please clear filters and try again.
           </p>
         </div>
       )}
-    </div>
+      {data.length > 0 && (
+        <div className="c-cards o-container o-container__texture">
+          <InfiniteScroll
+            dataLength={data.length}
+            next={() => {
+              loadMoreData();
+            }}
+            hasMore={true}>
+            <Row gutter={[25, 25]}>
+              {data?.map((superhero, index) => {
+                return (
+                  <Col span={6} xs={24} md={12} l={6} xl={6} key={index}>
+                    <Card superhero={superhero} key={index} />
+                  </Col>
+                );
+              })}
+            </Row>
+          </InfiniteScroll>
+        </div>
+      )}
+    </>
   );
 }
 
