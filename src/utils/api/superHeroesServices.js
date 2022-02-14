44b01b3ad24db7d91cd34f337551910c3a140fc4
Enrique Casanova <enrique.casanova@projekt202.com>
2022-02-14 07:17:55 -0500

    fix: change api call to read json with all data

--- src/utils/api/superHeroesServices.js
+++ src/utils/api/superHeroesServices.js
@@ -1,12 +1,33 @@
 import {axios} from '@adk/apiclient';
 import Client from './apiClient';
-
+import jsonData from '@/data.json';
 class SuperheroeService extends Client {
+  /* FAKE API CALL FROM JSON*/
   async getList() {
+    let data = jsonData;
+    return data;
+  }
+
+  /* FAKE API CALL FROM JSON*/
+  async getListApi() {
     let {data} = await axios.get(
       'https://www.superheroapi.com/api.php/10158234183617397/search/a',
     );
     return data;
+  }
+
+  /* GET ALL DATA FROM API*/
+  async getAllData() {
+    let list = {results: []};
+    for (let i = 1; i <= 731; i++) {
+      let {data} = await axios.get(
+        `https://www.superheroapi.com/api.php/10158234183617397/ + ${i}`,
+      );
+      delete data.response;
+      list.results.push(data);
+    }
+    console.log(list);
+    return list;
   }
 
   async getDetails(superHeroeId) {
